a6061ad5bf0e301ed763156e433eb188b2bace06
Jonathan Neidel <jneidel@jneidel.com>
2021-06-03 11:16:15 +0200

    Add boostrap env variable
    Update documentation
    Add --debug flag
    Fix start-prod script

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # p2p
+
+## Run
+
+Development:
+
+```sh
+npm start
+```
+
+Production:
+
+```sh
+npm run start-prod
+```
+
+## Configure
+
+Configured via environmental variables in `variables.env`.
+
+**PORT** - port for the peer to run on
+
+Example:
+
+```sh
+PORT=8070
+```
+
+**BOOSTRAP_LIST** - list of peers, to join the network on
+
+Format:
+  - `/ip4/IP/tcp/PORT`, example: `/ip4/127.0.0.1/tcp/4001`, [see transport pkg](https://github.com/libp2p/js-libp2p-tcp#transport)
+  - seperated by commas
+
+Example:
+
+```sh
+BOOTSTRAP_LIST=/ip4/127.0.0.1/tcp/8071, /ip4/127.0.0.1/tcp/8072
+```
+
+**Debugging environmental variables**
+
+Pass the `--debug` flag to show what env variables are being read in.
+
+```sh
+node app.js --debug
+```
--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const logger = require( "./logs/logger" );
 
 const app = express();
 
-require( "dotenv" ).config( { path: "variables.env" } );
-
-const { PORT } = process.env;
-
 app.use( logger.dev );
 
 app.use( bodyParser.json() );
@@ -33,6 +29,22 @@ app.use( ( err, req, res, next ) => {
   res.status( err.status || 500 );
   res.json( data );
 } );
+
+require( "dotenv" ).config( { path: "variables.env" } );
+const { PORT, BOOTSTRAP_LIST } = process.env;
+const port = PORT ? PORT : 9000;
+const bootstrapList = (BOOTSTRAP_LIST ? BOOTSTRAP_LIST : "").split( "," ).map( x => x.trim() ).filter( x => !!x );
+
+if ( process.argv.includes( "--debug" ) ) {
+  console.log( `Read environmental variables:
+
+  PORT: ${port}
+  BOOTSTRAP_LIST: ${bootstrapList.map( x => "\n    " + x )}
+` );
+  process.exit(0);
+}
+
+p2p( bootstrapList );
 
 app.listen( PORT, () => {
   console.log( `Server running on http://localhost:${PORT}` ); // eslint-disable-line no-console
