5c18dc51211de9d9d5e4d56cf2d40089b955a19e
Jonathan Neidel <jneidel@jneidel.com>
2021-06-04 11:43:59 +0200

    Integrate p2p node into express

--- app.js
+++ app.js
@@ -1,42 +1,46 @@
 const express = require( "express" );
 const bodyParser = require( "body-parser" );
 const logger = require( "./logs/logger" );
-const p2p = require( "./p2p" );
+const { createNode } = require( "./p2p/node" );
 const parseEnv = require( "./lib/parseEnv" );
 
 require( "dotenv" ).config( { path: "variables.env" } );
 const env = parseEnv( process.env );
 
-const app = express();
+(async () => { // async wrapper to be able to use await
+  const app = express();
+  app.use( logger.dev );
+  app.use( bodyParser.json() );
+  app.use( bodyParser.urlencoded( { extended: true } ) );
 
-app.use( logger.dev );
+  const node = await createNode( env );
+  app.use( ( req, res, next ) => {
+    req.node = node;
+    next();
+  } );
 
-app.use( bodyParser.json() );
-app.use( bodyParser.urlencoded( { extended: true } ) );
+  app.use( "/", require( "./routes" ) );
 
-app.use( "/", require( "./routes" ) );
+  process.on( "unhandledRejection", ( err ) => { throw err; } );
 
-process.on( "unhandledRejection", ( err ) => { throw err; } );
+  // 404
+  app.use( ( req, res, next ) => {
+    const err = new Error( "Not Found" );
+    err.status = 403;
+    next( err );
+  } );
 
-// 404
-app.use( ( req, res, next ) => {
-  const err = new Error( "Not Found" );
-  err.status = 403;
-  next( err );
-} );
+  // error handler
+  app.use( ( err, req, res, next ) => {
+    const data = {
+      error   : true,
+      errorMsg: err.message,
+    };
+    res.status( err.status || 500 );
+    res.json( data );
+  } );
 
-// error handler
-app.use( ( err, req, res, next ) => {
-  const data = {
-    error   : true,
-    errorMsg: err.message,
-  };
-  res.status( err.status || 500 );
-  res.json( data );
-} );
-
-p2p( env );
-
-app.listen( env.HTTP_PORT, () => {
-  console.log( `Server running on http://localhost:${env.HTTP_PORT}` ); // eslint-disable-line no-console
-} );
+  app.listen( env.HTTP_PORT, () => {
+    console.log( `Server running on http://localhost:${env.HTTP_PORT}` ); // eslint-disable-line no-console
+  } );
+})()
