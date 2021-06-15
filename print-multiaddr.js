26fa7dcd05a5e5952969ad62c9c69e3463a0ae1c
Jonathan Neidel <jneidel@jneidel.com>
2021-06-15 15:27:49 +0200

    Add script to print multiaddr

---
+++ print-multiaddr.js
@@ -0,0 +1,11 @@
+const parseEnv = require( "./lib/parseEnv" );
+const { getPeerId } = require( "./p2p/fs" );
+
+(async () =>  {
+  require( "dotenv" ).config( { path: "variables.env" } );
+  const env = parseEnv( process.env );
+
+  const peerId = await getPeerId( env.PEER_STORAGE );
+
+  console.log( `/ip4/${env.PEER_IP}/tcp/${env.PEER_PORT}/ipfs/${peerId.toB58String()}` )
+})()
